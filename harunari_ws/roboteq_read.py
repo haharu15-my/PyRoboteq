0e715aa4f929ecb7066aa279bc63a51835b5ec4d
haharu15-my <harunarikumamom@gmail.com>
2025-11-26 11:45:51 +0900

    11/26

--- harunari_ws/roboteq_read.py
+++ harunari_ws/roboteq_read.py
@@ -7,7 +7,8 @@ controller = RoboteqHandler(debug_mode=True, exit_on_interrupt=True)
 # 接続を確認  
 if controller.connect("COM3"):  # ポート名を適切に設定  
     print("接続成功")
-    READ = input("読みたいコマンドを入力してください: ").strip()
+    user_input = input("コマンド名を入力 (例: READ_MOTOR_AMPS): ")
+    command = getattr(cmds, user_input)
     result = controller.read_value(cmds.READ,1)  
     print(f"測定結果: {result}")
 else:  
