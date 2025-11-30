57f75662327b52104682ca3522877b10755bb672
haharu15-my <harunarikumamom@gmail.com>
2025-11-30 18:05:22 +0900

    新しいモジュールを追加し、キーボード入力によるロボットの移動制御機能を実装

---
+++ harunari_ws/move_proto copy.py
@@ -0,0 +1,40 @@
+from PyRoboteq import RoboteqHandler
+from PyRoboteq import roboteq_commands as cmds
+import time
+import keyboard
+
+controller = RoboteqHandler()
+connected = controller.connect("COM3")
+
+controller.send_command(cmds.REL_EM_STOP)# 電子緊急停止を解除
+
+if __name__ == "__main__":
+    print("Press D to drive")
+    print("Press S to stop")
+    while connected:
+        motor_rpm = controller.read_value(cmds.READ_BL_MOTOR_RPM)
+        current_speed_motor1 = controller.read_value(cmds.READ_BL_MOTOR_RPM, 1)  
+        current_speed_motor2 = controller.read_value(cmds.READ_BL_MOTOR_RPM, 2)
+
+        if keyboard.is_pressed('d'):
+            #print("S pressed")
+            drive_speed_motor_one = -200
+            drive_speed_motor_two  = -200
+            current_speed_motor1 = -200
+            current_speed_motor2 =- 200
+
+            if current_speed_motor1 == 0 and current_speed_motor2 == 0:
+
+                drive_speed_motor_one = -300
+                drive_speed_motor_two  = -300
+                print("-300")
+            
+        if keyboard.is_pressed('s'):
+            #print("S pressed")
+            drive_speed_motor_one = 0
+            drive_speed_motor_two  = 0
+
+        
+        controller.send_command(cmds.DUAL_DRIVE,drive_speed_motor_one, drive_speed_motor_two)
+        print(motor_rpm)
+#制作途中
